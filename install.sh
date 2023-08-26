7d57caebe286b1c073d2a6af66bbe479bdcfd3f3
Jet <taijunjet@hotmail.com>
2023-08-26 02:32:08 +0100

    modify install script

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# clone repo
+git clone git@github.com:jjshoots/mononoki-monospaced-patched.git
+
+# copy to the fonts dir
+cd mononoki-monospaced-patched
 mkdir -p ~/.local/share/fonts/Nerdfonts/
 cp ./*.ttf ~/.local/share/fonts/Nerdfonts/
+
+# delete the repo
+rm -rf mononoki-monospaced-patched
